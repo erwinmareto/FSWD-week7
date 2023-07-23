b1122966f724cb779c555df36bcfc1400a7fa0bc
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2023-07-23 11:26:08 +0700

    add text

--- soal1.js
+++ soal1.js
@@ -5,8 +5,8 @@ let squareSides = 2;
 let rectangleLength = 2;
 let rectangleWidth = 3;
 
-console.log(formula.squareArea(squareSides));
-console.log(formula.squarePerimeter(squareSides));
+console.log(`Keliling persegi: ${formula.squareArea(squareSides)}`);
+console.log(`Luas persegi: ${formula.squarePerimeter(squareSides)}`);
 
-console.log(formula.rectangleArea(rectangleLength, rectangleWidth));
-console.log(formula.rectanglePerimeter(rectangleLength, rectangleWidth));
+console.log(`Keliling persegi panjang: ${formula.rectangleArea(rectangleLength, rectangleWidth)}`);
+console.log(`Luas persegi panjang: ${formula.rectanglePerimeter(rectangleLength, rectangleWidth)}`);
